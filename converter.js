09220f3fb1827ab9d1fc30a0fda5ba1bc02a745a
LJ322 <general00760@gmail.com>
2020-07-19 01:00:14 +0300

    Renamed styles by BEM. Added some comments to the script. Added check for negative input value. Added rounding for numbers 0.55 - 0.58

--- converter.js
+++ converter.js
@@ -1,14 +1,22 @@
 'use strict'
 
-function convert() {
-  // Declare variables for input value and message
-  let message, num = document.getElementById('number').value;
-  // Delete previous result
-  if (document.querySelector('table')) document.querySelector('table').remove();
-  if (document.querySelector('.total')) document.querySelector('.total').remove();
+let btn_convert = document.querySelector('.app__btn-convert');
+btn_convert.addEventListener('click', convert);
 
-  // Coin counting function
-  function divide_by_coins(number) {
+function convert() {
+  // Declare obj for input value and further output value
+  let io = {
+    input: document.querySelector('.app__input').value,
+    output: ''
+  }
+  // Get main element
+  let app = document.querySelector('.app');
+  // Delete previous result
+  if (document.querySelector('.app-table')) document.querySelector('.app-table').remove();
+  if (document.querySelector('.app__output')) document.querySelector('.app__output').remove();
+
+  // Coin counting function. Splite cents into different coins
+  function split_by_coins(number) {
     let penny, nickel, dime, quarter, balance;
     quarter = Math.floor(number / 25);
     balance = number % 25;
@@ -17,38 +25,45 @@ function convert() {
     nickel = Math.floor(balance / 5);
     balance = balance % 5;
     penny = Math.floor(balance / 1);
-    return `<table>
-              <tr>
-                <td>Coins</td>
-                <td>Count</td>
+    return `<table class="app-table">
+              <tr class="app-table__row">
+                <td class="app-table__cell">Coins</td>
+                <td class="app-table__cell">Count</td>
               </tr>
-              <tr>
-                <td>Quarters (25 &#162)</td><td>${quarter}</td>
+              <tr class="app-table__row">
+                <td class="app-table__cell">Quarters (25 &#162)</td>
+                <td class="app-table__cell">${quarter}</td>
               </tr>
-              <tr>
-                <td>Dimes (10 &#162)</td><td>${dime}</td>
+              <tr class="app-table__row">
+                <td class="app-table__cell">Dimes (10 &#162)</td>
+                <td class="app-table__cell">${dime}</td>
               </tr>
-              <tr>
-                <td>Nickels (5 &#162)</td><td>${nickel}</td>
+              <tr class="app-table__row">
+                <td class="app-table__cell">Nickels (5 &#162)</td>
+                <td class="app-table__cell">${nickel}</td>
               </tr>
-              <tr>
-                <td>Pennies (1 &#162)</td><td>${penny}</td>
+              <tr class="app-table__row">
+                <td class="app-table__cell">Pennies (1 &#162)</td>
+                <td class="app-table__cell">${penny}</td>
               </tr>
             </table>`;
   }
 
-  // Check input value
-  if (!(Number.isFinite(+num))) message = `<div class="total">This is incorrect value</div>`;
+  // Check input value. If input value not number or negative number
+  if (!(Number.isFinite(+io.input)) || io.input.indexOf('-') != -1) io.output = `<p class="app__output">This is incorrect value</p>`;
   else {
+    // Declare variable for cents
     let cents;
-    if (num.indexOf('.') != -1 && num.indexOf('.') < num.length-3) {
-      message = `<div class="total">This is incorrect value</div>`;
+    // If input value not integer, but the balance is over 99 cents
+    if (io.input.indexOf('.') != -1 && io.input.indexOf('.') < io.input.length-3) {
+      io.output = `<p class="app__output">This is incorrect value</p>`;
     } else {
-      cents = +num * 100;
-      message = `<div class="total">Total cents: ${cents}</div>` + divide_by_coins(cents);
+      // Convert dollars to cents. Math.round() used for rounding numbers 0.55 - 0.58
+      cents = Math.round(+io.input * 100);
+      io.output = `<p class="app__output">Total cents: ${cents}</p>` + split_by_coins(cents);
     }
-
   }
-  // Create block for function's message
-  document.querySelector('.window').insertAdjacentHTML('beforeend', message);
+
+  // Render output
+  app.insertAdjacentHTML('beforeend', io.output);
 }
